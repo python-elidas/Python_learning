a7d42aaf217598d713b35381a93be02a206ea3fb
osgumu <osgumu-@hotmail.com>
2025-01-22 20:27:44 +0100

    Herencia y ejercicios

---
+++ 99.Ejercicios/ejercicio_calculadora.py
@@ -0,0 +1,51 @@
+'''
+Author: 
+Email: 
+Python version: 
+Date: 
+Version: 
+'''
+
+'''
+Crear una clase calculadora que tomando dos valores,  sea capaz de realizar las siguientes operaciones:
+- Suma
+- Resta
+- Multiplicación
+- División
+- Potencia
+Extras:
+- Multiplicación recursiva
+- División con try except
+- Potencia recursiva
+'''
+
+class calculadora:
+    def __init__(self):
+        pass
+    
+    def suma(self):
+        pass
+    
+    def resta(self):
+        pass
+    
+    def multiplicacion(self):
+        pass
+    
+    def multiplicacio_recursiva(self):
+        pass
+    
+    def division(self):
+        pass
+    
+    def division_try_except(self):
+        pass
+    
+    def potencia(self):
+        pass
+    
+    def potencia_recursiva(self):
+        pass
+    
+    def raiz_cuadrada(self):
+        pass
